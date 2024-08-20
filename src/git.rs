56c9d2b3fd665b2099a91d3ec3cba3a58da07308
Javier Muñoz Tous <javimtib92@gmail.com>
2024-08-20 21:35:13 +0200

    feat: implement get commits within a range of commits

--- src/git.rs
+++ src/git.rs
@@ -61,3 +61,39 @@ pub fn get_tags(branch: &str) -> Vec<String> {
 
     tags
 }
+
+pub fn get_commits(from: &str, to: &str) -> Vec<String> {
+    let repo = match Repository::open_from_env() {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to clone: {}", e),
+    };
+
+    let mut revwalk = repo.revwalk().expect("Couldn\'t retrieve revwalk");
+
+    let from =
+        Oid::from_str(from).expect(&format!("from parameter \"{:?}\" is not a valid SHA", from));
+
+    let to = Oid::from_str(to).expect(&format!("to parameter \"{:?}\" is not a valid SHA", to));
+
+    revwalk.push(to).expect(&format!(
+        "Couldn\'t set revwalk root to commit \"{:?}\"",
+        to
+    ));
+
+    revwalk
+        .hide(from)
+        .expect(&format!("Couldn\'t hide commit \"{:?}\"", from));
+
+    let mut commits = Vec::new();
+
+    for oid in revwalk {
+        if let Ok(oid) = oid {
+            let commit = repo.find_commit(oid).expect("Couldn\'t find commit");
+            if let Some(message) = commit.message() {
+                commits.push(message.to_string());
+            }
+        }
+    }
+
+    commits
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use semantic_release::git::{get_tag_head, get_tags};
+use semantic_release::git::{get_commits, get_tag_head, get_tags};
 
 fn main() {
     get_tag_head("v0.0.5");
 
     get_tags("origin/release-v0.0.15");
     get_tags("origin/release-v0.0.9");
+
+    get_commits(
+        "0779705ecc46cbced5059bcbadee7b8d254d4300",
+        "3d92276063e6ebb33d63e2d20bf23d405f9d4925",
+    );
 }
